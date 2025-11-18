83cafc966e0e57d3dc60c9fe7d868a549e8b6aea
Santiago Zapata <dev.santizapata@gmail.com>
2025-11-18 15:21:00 -0500

    Solución problema registrado #1
    Lo que hicimos fue hacer correr el programa directamente si la variale programa no es igual a 2, que es la opcion para salir, y despues que se le solicita el valor al usuario se verifica que si este dentro los valores permitidos.

--- historiaUsuario.py
+++ historiaUsuario.py
@@ -1,18 +1,21 @@
-print("Registro de productos\n 1 - Registrar producto\n 2 - Salir")
-programa = int(input("Que quieres hacer?: "))
+programa = 0
 
-while programa < 1 or programa > 2:
-    print("Ingresa una opción valida")
-    programa = int(input("Que quieres hacer?: "))
-else:
-    while programa == 1:
+while programa != 2:
+    try:
+        print("Registro de productos\n 1 - Registrar producto\n 2 - Salir")
+        programa = int(input("Que quieres? "))
+        print("")
+    except:
+        print("Agrega una opción valida\n")
+    if programa < 1 or programa > 2:
+        print("Ingresa un valor valido\n")
+    if programa == 1:
         try: 
             nombre = input("Ingresa el nombre del producto: ")
             precio = float(input("Ingresa el precio del producto: "))
             cantidad = int(input("Ingresa la cantidad del producto que tienes: "))
+            print("\n")
         except:    
-            print("Ingresa correctamente los valores (letras, decimal y entero)")
+                print("Ingresa correctamente los valores (letras, decimal y entero)")
         costo_total = precio * cantidad
-        print(f"Nombre del producto: {nombre}\nPrecio unitario: {precio}\nCantidad: {cantidad}\nCosto total calculado: {costo_total}")
-        print("Registro de productos\n 1 - Registrar producto\n 2 - Salir")
-        programa = int(input("Que quieres hacer?: "))
+        print(f"Nombre del producto: {nombre}\nPrecio unitario: {precio}\nCantidad: {cantidad}\nCosto total calculado: {costo_total}\n")
